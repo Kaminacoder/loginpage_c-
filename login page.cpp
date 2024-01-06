40d391362cb9ee1bbb9cf1fcf463d291beca0166
Rahul Yadav <89114621+Kaminacoder@users.noreply.github.com>
2024-01-06 12:55:03 +0530

    Add files via upload

---
+++ login page.cpp
@@ -0,0 +1,115 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+class temp{
+	string userName, Email, password;
+	string searchName, searchPass, searchEmail;
+	fstream file;
+	public:
+		void login();
+		void singup();
+		void forgot();
+}obj;
+int main (){
+	char choice;
+	cout<<"\n1- Login";
+	cout<<"\n2- Sign-up";
+	cout<<"\n3- Forgot Password";
+	cout<<"\n4- Exit";
+	cout<<"\Enter Your Choice ::";
+	cin>>choice;
+	
+	switch(choice){
+		case '1':
+			cin.ignore();
+			obj.login();
+		break;
+		case '2':
+			cin.ignore();
+			obj.singup();
+		break;
+		case '3':
+			cin.ignore();
+			obj.forgot();
+		break;
+		case '4':
+			return 0;
+		break;
+		defualt:
+			cout<< "Invalid Selection...!";
+		
+	}
+}
+void temp ::singup(){
+	cout<<"\nEnter Your User Name ::";
+	getline(cin,userName);
+	cout<<"\nEnter Your Email Address ::";
+	getline(cin,Email);
+	cout<<"\nEnter Your Password ::";
+	getline(cin,password);
+	
+	file.open("loginData.txt" ,ios ::out| ios ::app);
+	file<<userName<<"*"<<Email<<"*"<<password<<endl;
+	file.close();
+	
+}
+void temp ::login(){
+	cout<<"________________LOGIN______________"<<endl;
+	cout<<"\nEnter Your User Name ::"<<endl;
+	getline(cin,userName);
+	cout<<"\nEnter Your Password ::"<<endl;
+	getline(cin,searchPass);
+	
+	file.open("loginData.txt" ,ios ::in);
+	getline(file,userName,'*');
+	getline(file,Email,'*');
+	getline(file,password,'\n');
+	while(!file.eof()){
+		if(userName == searchName){
+			if(password == searchPass){
+				cout<<"\nAccount Login Sucesfull.....!";
+				cout<<"\nUsername ::" <<userName<<endl;
+				cout<<"\nEmail ::"<<Email<<endl;
+			
+			}
+			else{
+				cout<<"Password is Incorrect...!";
+			}
+	
+		}
+	getline(file,userName,'*');
+	getline(file,Email,'*');
+	getline(file,password,'\n');
+	}
+	file.close();
+	
+}
+void temp :: forgot(){
+    cout<<"\nEnter Your UserName :: ";
+    getline(cin,searchName);
+    cout<<"\nEnter Your Email Address :: ";
+    getline(cin,searchEmail);
+
+    file.open("loginData.txt",ios :: in);
+    getline(file,userName,'*');
+    getline(file,Email,'*');
+    getline(file,password,'\n');
+    while(!file.eof()){
+        if(userName == searchName){
+            if(Email == searchEmail){
+                cout<<"\nAccount Found...!"<<endl;
+                cout<<"Your Password :: "<<password<<endl;
+            }else{
+                cout<<"Not found...!\n";
+            }
+        }else{
+            cout<<"\nNot fount...!\n";
+        }
+    getline(file,userName,'*');
+    getline(file,Email,'*');
+    getline(file,password,'\n');
+    }
+    file.close();
+}
+
+
